2199961237d7754c0c5f7deaecb4ba2350021413
Abhishek Prasad Seth (Personal) <abhishekpseth@gmail.com>
2026-03-30 01:27:07 +0530

    added thingsILearned File

---
+++ src/ThingsILearned.java
@@ -0,0 +1,8 @@
+public class ThingsILearned {
+//    Things i learned ->
+//        1. code likhne k baad use upar se neeche khud se verify kro just like pr banana se Pehle krte ho
+//        2. dekho aur kitne variation, and refactoring kr skte ho. no that ki it just worked
+//        3. in java, character array and string are different. u cant create a string when declaring for character array.
+//        4. while loop can be changed to recursion and vice versa
+//        5. Quick Select Algorithm when u have to partition into two part (for example smaller element on left side and larger on the right side)
+}
