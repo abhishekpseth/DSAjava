7fe1d944e030a04d884233b12a55a96749bf5100
Abhishek Prasad Seth (Personal) <abhishekpseth@gmail.com>
2026-03-29 21:26:20 +0530

    Updated Quick sort with another partition algorithm where pivot is selected at mid-point instead of left most

--- src/SearchingAndSorting/QuickSort.java
+++ src/SearchingAndSorting/QuickSort.java
@@ -6,6 +6,35 @@ public class QuickSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    public static int partition2(int[] arr, int l, int r){
+        int pivot = (l + r) >> 1;
+
+        swap(arr, pivot, r);
+        pivot = r;
+
+        // Now here, write an algorithm which finds the correct position for an element in an array
+        int i = l; int j = r-1;
+
+        while(i <= j){
+            while(i <= j && arr[i] <= arr[pivot])i++;
+            while(i <= j && arr[j] >= arr[pivot])j--;
+
+            if(i <= j){
+                swap(arr, i, j);
+            }
+        }
+
+        // so here i will be at correct position
+        swap(arr, i, pivot);
+
+        for(int it : arr){
+            System.out.println(it);
+        }
+
+        return i;
+    }
+
     static int partition(int[] arr, int l, int r){
         int pivot = arr[l];
 
