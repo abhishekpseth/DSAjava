6f340ab40e95c738163b0ffca6c382ea8ed02ec6
Abhishek Prasad Seth (Personal) <abhishekpseth@gmail.com>
2026-04-23 01:16:49 +0530

    added problem two sum

---
+++ src/Array/Ques18_TwoSum.java
@@ -0,0 +1,48 @@
+package Array;
+
+import java.util.*;
+
+public class Ques18_TwoSum {
+    public static ArrayList<ArrayList<Integer>> getPairs(int[] arr) {
+        // code here
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for (int num : arr) {
+            mp.put(num, mp.getOrDefault(num, 0) + 1);
+        }
+
+        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+
+            if(key == 0 && val > 1){
+                // res.add(Arrays.asList(0, 0));
+                res.add(new ArrayList<>(Arrays.asList(0, 0)));
+            } else if(key < 0){
+                Integer pos = (-1)*key;
+                Integer neg = key;
+
+                if(mp.containsKey(pos)) {
+                    ArrayList<Integer> pair = new ArrayList<>();
+                    pair.add(neg);
+                    pair.add(pos);
+
+                    res.add(pair);
+                }
+            }
+        }
+
+        Collections.sort(res, (list1, list2) -> list1.get(0).compareTo(list2.get(0)));
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        ArrayList<ArrayList<Integer>> res = getPairs(arr);
+
+        System.out.println(res);
+    }
+}
