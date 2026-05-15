144f04b30e85bd578e308ef6edda481682d7950b
Abhishek Prasad Seth (Personal) <abhishekpseth@gmail.com>
2026-05-15 22:02:54 +0530

    Started HashMaps, added problem countFrequency

---
+++ src/HashMap/CountFrequency.java
@@ -0,0 +1,33 @@
+package HashMap;
+
+import java.util.*;
+
+public class CountFrequency {
+    public static ArrayList<ArrayList<Integer>> countFreq(int[] arr) {
+        // code here
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        for(int it : arr) {
+            mp.put(it, mp.getOrDefault(it, 0) + 1);
+        }
+
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+
+        for(Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            ArrayList<Integer> internal = new ArrayList<>();
+
+            internal.add(entry.getKey());
+            internal.add(entry.getValue());
+
+            res.add(internal);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 5};
+
+        countFreq(arr);
+    }
+}
